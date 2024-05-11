0e00bbb997a4574b79bc7a34e378f6527db991c9
Cynos28 <sendtocynos@gmail.com>
2024-05-12 03:43:33 +0530

    images  replace

--- app/src/main/java/com/example/focusminder/Login.kt
+++ app/src/main/java/com/example/focusminder/Login.kt
@@ -1,11 +1,20 @@
 package com.example.focusminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val navigateToLoginButton = findViewById<TextView>(R.id.registerText)
+        navigateToLoginButton.setOnClickListener { // Navigate to login page
+            val intent = Intent(this@Login, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/focusminder/Register.kt
+++ app/src/main/java/com/example/focusminder/Register.kt
@@ -1,11 +1,21 @@
 package com.example.focusminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Register : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val navigateToLoginButton = findViewById<TextView>(R.id.LoginText)
+        navigateToLoginButton.setOnClickListener { // Navigate to login page
+            val intent = Intent(this@Register, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
